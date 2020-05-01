141c2df0387261d985781dd4174518dcb6d6d27f
sahana1563 <64599445+sahana1563@users.noreply.github.com>
2020-05-01 09:09:19 +0530

    grade.c

---
+++ grade.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+float marks;
+printf("enter marks:");
+scanf("%f",&marks);
+if(a<=100&&a>=85)
+printf("grade-A");
+elseif(a<=84&&a>=70)
+printf("grade-B");
+elseif(a<=69&&a>=55)
+printf("grade-C");
+elseif(a<=54&&a>=40)
+printf("grade-D");
+else
+printf("FAIL");
+return 0;
+}
